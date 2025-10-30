0485a13451664b85b9ce191d64aea928660c8c1b
Fernando Silva <fernandophes@gmail.com>
2025-10-30 12:35:31 -0300

    Exceção base para criptografia

---
+++ common/src/main/java/br/edu/ufersa/cc/seg/common/crypto/CryptoException.java
@@ -0,0 +1,14 @@
+package br.edu.ufersa.cc.seg.common.crypto;
+
+/**
+ * Exceção para erros relacionados à criptografia
+ */
+public class CryptoException extends RuntimeException {
+    public CryptoException(String message) {
+        super(message);
+    }
+
+    public CryptoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
