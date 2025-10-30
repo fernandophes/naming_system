176bc72b804d7cb39f7c5a44bb21a845efca1a6f
Fernando Silva <fernandophes@gmail.com>
2025-10-30 11:04:53 -0300

    Commit inicial

---
+++ src/main/java/br/edu/ufersa/cc/seg/naming_system/NamingSystemApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.cc.seg.naming_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingSystemApplication.class, args);
+	}
+
+}
