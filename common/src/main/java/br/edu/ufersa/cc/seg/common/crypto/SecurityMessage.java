7fcf8ef5f951c28f4ff1d0bb1eef688a0a561db0
Fernando Silva <fernandophes@gmail.com>
2025-10-30 12:22:55 -0300

    Feat: Objeto para encapsular as mensagens

---
+++ common/src/main/java/br/edu/ufersa/cc/seg/common/crypto/SecurityMessage.java
@@ -0,0 +1,34 @@
+package br.edu.ufersa.cc.seg.common.crypto;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Representa uma mensagem segura que inclui o conteúdo cifrado,
+ * HMAC para integridade/autenticidade, e outros metadados necessários.
+ */
+@Data
+@Builder
+public class SecurityMessage {
+
+    /**
+     * Conteúdo cifrado com algoritmo simétrico
+     */
+    private byte[] encryptedContent;
+
+    /**
+     * HMAC para integridade e autenticidade
+     */
+    private byte[] hmac;
+
+    /**
+     * Vetor de inicialização para cifra
+     */
+    private byte[] iv;
+
+    /**
+     * Timestamp para evitar replay attacks
+     */
+    private long timestamp;
+
+}
