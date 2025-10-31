ffbf8fcf0a897b76a55b346f1c5f881d9608172e
Fernando Silva <fernandophes@gmail.com>
2025-10-30 22:42:58 -0300

    Revert "Remoção de código inutilizado"
    This reverts commit fb30821818a5ef3176f5cbee2dd218c5ebc5227a.

---
+++ src/test/java/br/edu/ufersa/cc/seg/naming_system/NamingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.cc.seg.naming_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
