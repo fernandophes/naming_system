7f105c05b9974f0a02878da825294e8871d2cbb0
Fernando Silva <fernandophes@gmail.com>
2025-11-03 23:02:14 -0300

    Correção do launcher do mini DNS

--- mini-dns/src/main/java/br/edu/ufersa/cc/seg/dns/MiniDnsApplication.java
+++ mini-dns/src/main/java/br/edu/ufersa/cc/seg/dns/MiniDnsApplication.java
@@ -7,15 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 public class MiniDnsApplication {
 
     private static final int PORT = 9000;
-    private static final DnsServer DNS_SERVER = new DnsServer(PORT);
 
     public static void main(final String[] args) {
-        // Inicia o servidor DNS em porta 9000
-        final var serverThread = new Thread(DNS_SERVER::start);
-        serverThread.setDaemon(true);
-        serverThread.start();
-
-        log.info("Mini DNS iniciado na porta 9000");
+        new DnsServer(PORT).start();
     }
 
 }
